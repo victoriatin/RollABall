d4a99f7f50846e4555d6fe1e34cab0c40e4c1a48
victoriatin <52183531+victoriatin@users.noreply.github.com>
2019-07-08 10:26:53 -0400

    Completed Tier 3

---
+++ Roll a Ball/Assets/Scripts/Block.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+   
+  public float speed = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+             //PingPong between 0 and 1
+       
+          transform.position = new Vector3(Mathf.PingPong(Time.time, 3), transform.position.y, transform.position.z);
+    }
+}
